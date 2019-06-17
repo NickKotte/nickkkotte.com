fb0e07132d94c216f699a98f925628688b9bfe78
NickKotte <nickkotte@gmail.com>
2019-06-17 12:44:39 -0400

    Updated visuals for randomizer

--- js/wikipedia.js
+++ js/wikipedia.js
@@ -5,28 +5,54 @@
 $(document).ready(function(){
 
 
-  var main = function(){
+    $("#wikipedia h1").hover(
+        function(){
+            $(this).css("color","#e8491d").css("fontSize","19px");
+        },
+        function(){
+            $(this).css("color","white").css("fontSize","18px");
+        }
+    );
+
+    $("#wikipedia h1").on("click", function(){
     $.getJSON("https://api.nasa.gov/planetary/apod?api_key=EVooq5ktpP7fFB9rSffhzdChbzCvB5TA6c2zEba8", function(json){
         console.log(json);
 
         $(".showcase").toggle(800);
         if(json.media_type == "image"){
-          $("#imgID").attr("src", json.url).delay(400).attr("class","center").slideToggle(1200);
+          $("#imgID").attr("src", json.url).delay(400).attr("class","center").slideToggle(1000);
           $("#vidID").css("display", "none");
         }
         else{
-          $("#vidID").attr("src", json.url).delay(400).attr("class","center").slideToggle(1200);
+          $("#vidID").attr("src", json.url).delay(400).attr("class","center").slideToggle(1000);
           $("#imgID").css("display", "none");
         }
-        $("#titleID").html(json.title);
-          $("#titleID").slideToggle(1200);
+
+        var cr=" -";
+        if(json.copyright){
+            cr +=json.copyright;
+        }
+
+        $("#titleID").html(json.title+cr);
+        $("#titleID").slideToggle(1000);
         $("#descriptionID").html(json.explanation);
-        $("#descriptionID").slideToggle(1200);
+        $("#descriptionID").slideToggle(1000);
+
+        function scroll()
+        {
+            $('html, body').animate({
+                scrollTop: ($('#wikipedia').offset().top)
+            },1500);
+        }
+
+        setTimeout(scroll, 800);
+
+
 
     });
 
-  };
-  $("h1").on("click", main);
+});
+
 
 
 });
